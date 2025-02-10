db2c1b54d966187223bfc32c9e05b10eb70cfab4
EiMarina <mariivankova69@gmail.com>
2025-02-10 21:18:31 +0200

    Second Commit
    +commentaries

--- hospitalPatientJournal/src/hospitalPatientJournal/Hospital.java
+++ hospitalPatientJournal/src/hospitalPatientJournal/Hospital.java
@@ -6,33 +6,60 @@ public class Hospital {
 	private ArrayList<Patient> patients = new ArrayList<Patient>();
 	private ArrayList<Department> departments = new ArrayList<Department>();
 	
+	// Creating a new patient
 	public void addPatient(Patient patient) {
 		this.patients.add(patient);
 		System.out.println("Patient is added!");
 	}
 	
-	    // Search patients by family name       
-	    public ArrayList<Patient> searchByFamilyName(String familyName) {
-	         ArrayList<Patient> filteredPatients = new ArrayList<>();
+	// Deleting a patient by deleting his id
+	public void deletePatient(int id) {
+	    patients.removeIf(patient -> patient.getId() == id);
+	    System.out.println("Patient is deleted!");
+	}
+	
+	// Searching patients by family name
+	public void updatePatientStatus(int id, String newStatus) {
+        for (Patient patient : patients) {
+            if (patient.getId() == id) {
+                patient.setStatus(newStatus);
+                System.out.println("Patient status updated!");
+                patient.displayPatientInfo();
+                break;
+            }
+         }
+	  }
+
+		// Searching patients by family name       
+	   public ArrayList<Patient> searchByFamilyName(String familyName) {
+	        ArrayList<Patient> filteredPatients = new ArrayList<>();
 	            for (Patient patient : patients) {
-	               if (patient.getFamilyName().contains(familyName)) {
+	               if (patient.getFamilyName().equalsIgnoreCase(familyName)) {
 	            	   filteredPatients.add(patient);
 	               }
 	             }
 	             return filteredPatients;
 	             }
 
-	    // Search patients by diagnosis
+	    // Searching patients by diagnosis
 	    public ArrayList<Patient> searchByDiagnosis(String diagnosis) {
 	    	ArrayList<Patient> filteredPatients = new ArrayList<>();
 	             for (Patient patient : patients) {
-	            	 if (patient.getDiagnosis().contains(diagnosis)) {
+	            	 if (patient.getDiagnosis().equalsIgnoreCase(diagnosis)) {
 	            	   filteredPatients.add(patient);
 	               }
 	             }
 	             return filteredPatients;
 	            }
 	    
-	    // Search patients by department
-	   
-	}
+	    // Searching patients by department
+	    public ArrayList<Patient> searchByDepartment(Department department) {
+	        ArrayList<Patient> filteredPatients = new ArrayList<>();
+	        for (Patient patient : patients) {
+	            if (patient.getDepartment().equals(department)) {
+	                filteredPatients.add(patient);
+	            }
+	        }
+	        return filteredPatients;
+	    }
+}
--- hospitalPatientJournal/src/hospitalPatientJournal/Patient.java
+++ hospitalPatientJournal/src/hospitalPatientJournal/Patient.java
@@ -3,26 +3,30 @@ package hospitalPatientJournal;
 import java.util.Date;
 
 public class Patient {
-	private String id;
+	private int id;
 	private String familyName;
 	private String diagnosis;
 	private Date admissionDate;
+	private Department department;
 	private String status;
 	private static int COUNTER = 0;
 	
-	public Patient(String id, String familyName, String diagnosis, Date admissionDate, String status) {
+	// Constructor for the patient class
+	public Patient(String familyName, String diagnosis, Date admissionDate, Department department, String status) {
 		this.familyName = familyName;
-		this.id = " "+(++COUNTER);
+		this.id = ++COUNTER;
 		this.diagnosis = diagnosis;
 		this.admissionDate = admissionDate;
+		this.department = department;
 		this.status = status;
-}
-
-	public String getId() {
+    }
+	
+	// Getters and Setters
+	public int getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
@@ -50,19 +54,30 @@ public class Patient {
 		this.admissionDate = admissionDate;
 	}
 
-	public String status() {
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	public String getStatus() {
 		return status;
 	}
 
-	public void status(String status) {
+	public void setStatus(String status) {
 		this.status = status;
 	}
 	
+	// Method of displaying all the information about the patient
 	public void displayPatientInfo() {
 	      System.out.println("Patient ID: " + id);
 	      System.out.println("Family Name: " + familyName);
 	      System.out.println("Diagnosis: " + diagnosis);
 	      System.out.println("Admission Date: " + admissionDate);
+	      System.out.println("Department: " + department);
 	      System.out.println("Status: " + status);
-	    }
 	}
+}
+
