f78c3937aa0ccb65dccae2636baab3dc24ffa59a
EiMarina <mariivankova69@gmail.com>
2025-02-15 20:01:10 +0200

    Fourth Commit
    +search by birthyear and familyname. Check for prevention of nullpounterexceptions

--- hospitalPatientJournal/src/hospitalPatientJournal/Hospital.java
+++ hospitalPatientJournal/src/hospitalPatientJournal/Hospital.java
@@ -26,6 +26,7 @@ public class Hospital {
 		// Searching patients by family name       
 	 public ArrayList<Patient> searchByFamilyName(String familyName) {
 	    ArrayList<Patient> filteredPatients = new ArrayList<>();
+	    if (familyName == null) return filteredPatients;
 	       for (Patient patient : patients) {
 	           if (patient.getFamilyName().equalsIgnoreCase(familyName)) {
 	              filteredPatients.add(patient);
@@ -37,6 +38,7 @@ public class Hospital {
 	  // Searching patients by diagnosis
 	  public ArrayList<Patient> searchByDiagnosis(String diagnosis) {
 	     ArrayList<Patient> filteredPatients = new ArrayList<>();
+	     if (diagnosis == null) return filteredPatients;
 	       for (Patient patient : patients) {
 	           if (patient.getDiagnosis().equalsIgnoreCase(diagnosis)) {
 	              filteredPatients.add(patient);
@@ -48,11 +50,25 @@ public class Hospital {
 	  // Searching patients by department
 	  public ArrayList<Patient> searchByDepartment(Department department) {
 	     ArrayList<Patient> filteredPatients = new ArrayList<>();
+	     if (department == null) return filteredPatients;
 	       for (Patient patient : patients) {
 	           if (patient.getDepartment().equals(department)) {
 	               filteredPatients.add(patient);
 	               }
 	           }
 	           return filteredPatients;
-	   }
+	  }    
+	  
+	  // Searching patients by family name and year of birth
+	  public ArrayList<Patient> searchByFamilyNameAndBirthYear (String familyName, int birthYear) {
+		  ArrayList<Patient> filteredPatients = new ArrayList<>();    
+		  if (familyName == null) return filteredPatients;
+		    for (Patient patient : patients) {
+	            if (patient.getFamilyName().equalsIgnoreCase(familyName) && patient.getBirthYear() == birthYear) {
+	            	filteredPatients.add(patient);
+	            	}
+		        }
+	            return filteredPatients;
+	                
+	  }
 }
