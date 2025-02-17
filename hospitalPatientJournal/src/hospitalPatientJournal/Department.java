490e82687e8a02e830f903a707531fabcaad642c
EiMarina <mariivankova69@gmail.com>
2025-02-17 21:59:19 +0200

    Sixth commit
    Changes to DepartmentName

--- hospitalPatientJournal/src/hospitalPatientJournal/Department.java
+++ hospitalPatientJournal/src/hospitalPatientJournal/Department.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Department {
-	private String name; // Department name
+	private String departmentName; // Department name
 	private List<Patient> patients; // list of patients in the department
 
 	// Constructor
-	public Department(String name) {
-		this.name = name;
+	public Department(String departmentName) {
+		this.departmentName = departmentName;
 		this.patients = new ArrayList<>();
 	}
 
 	// Method adds patients to department
 	public void addPatient(Patient patient) {
 		patients.add(patient);
-		System.out.println("Patient " + patient.getFamilyName() + " added to the department " + name);
+		System.out.println("Patient " + patient.getFamilyName() + " added to the department " + departmentName);
 	}
 
 	// Method for deleting patients by ID
@@ -24,7 +24,7 @@ public class Department {
 		Patient patientToRemove = findPatientById(patientId);
 		if (patientToRemove != null) {
 			patients.remove(patientToRemove);
-			System.out.println("Patient with ID " + patientId + " removed from the department " + name);
+			System.out.println("Patient with ID " + patientId + " removed from the department " + departmentName);
 		} else {
 			System.out.println("Patient with ID " + patientId + " not found.");
 		}
@@ -38,9 +38,9 @@ public class Department {
 	// Method to print information about all patients in the department
 	public void printPatients() {
 		if (patients.isEmpty()) {
-			System.out.println("There are no patients in the department " + name + ".");
+			System.out.println("There are no patients in the department " + departmentName + ".");
 		} else {
-			System.out.println("List of patients in the department " + name + ":");
+			System.out.println("List of patients in the department " + departmentName + ":");
 			for (Patient patient : patients) {
 				System.out.println(patient);
 			}
